3469b471156ce42dbb582db4c687e94398bfcdc2
Bushstar <bushsolo@gmail.com>
2014-09-08 16:03:00 +0100

    Update splash screen

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -11,7 +11,7 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
 {
     // set reference point, paddings
     int paddingLeftCol2         = 145;
-    int paddingTopCol2          = 376;
+    int paddingTopCol2          = 25;
     int line1 = 0;
     int line2 = 13;
     int line3 = 26;
