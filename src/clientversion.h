b62b9b47b4489d9136dc46c47d9848c0615223ad
Peter Bushnell <bushsolo@gmail.com>
2018-05-11 10:14:59 +0100

    v0.8.8.0

--- src/clientversion.h
+++ src/clientversion.h
@@ -8,15 +8,15 @@
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       8
-#define CLIENT_VERSION_REVISION    7
-#define CLIENT_VERSION_BUILD       1
+#define CLIENT_VERSION_REVISION    8
+#define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2018
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
